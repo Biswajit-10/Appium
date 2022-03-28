3f2b926ad2875cd02a05889a9dcbae5d0a9c4678
Biswajit-10 <88229173+Biswajit-10@users.noreply.github.com>
2022-03-28 16:41:51 +0530

    Update A.java

--- src/test/java/io/TestPackage/A.java
+++ src/test/java/io/TestPackage/A.java
@@ -18,6 +18,36 @@ public class A {
 		
 		System.out.println(a.length);
 	}
-	
+	/**
+	*Run app in background and relaunch where you left
+	*
+	**/
+	public void b(){
+	IOSDriver driver = new IOSDriver(url,cap);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElementById("JD-SBYONO").click();
+		driver.runAppInBackground(Duration.ofSeconds(-1));
+		driver.runAppInBackground(Duration.ofSeconds(10));
+		
+//		Thread.sleep(10);
+		driver.launchApp();
+	}
+	/**
+	*Activates the given app if it installed, but not running or if it is running in the background.
+	*relaunch where you left
+	**/
+	public void c(){
+	IOSDriver driver = new IOSDriver(url,cap);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElementById("JD-SBYONO").click();
+		driver.runAppInBackground(Duration.ofSeconds(-1));
+		
+//		driver.activateApp("bundleId", "com.sbi.lotusintouch.internal");
+		driver.activateApp("com.sbi.lotusintouch.internal");
+		Thread.sleep(5000);
+		driver.activateApp("com.apple.MobileSMS");
+	}
 }
 
