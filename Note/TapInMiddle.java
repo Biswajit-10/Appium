118ab0986a1dbf8d4abddef754dba0a83de8d47e
Biswajit-10 <88229173+Biswajit-10@users.noreply.github.com>
2022-04-16 00:21:04 +0530

    Create TapInMiddle.java

---
+++ Note/TapInMiddle.java
@@ -0,0 +1,14 @@
+class TapInMiddle{
+  /* From carina Framework*/
+   public void tapInMiddle(AppiumDriver driver ,WebElement element) {
+			   Point location = element.getLocation();
+			   int x=location.getX();
+			   int y=location.getY();
+			   Dimension size = element.getSize();
+			   int height = size.getHeight();
+			   int width = size.getWidth();
+			   TouchAction action=new TouchAction(driver);
+			   action.tap(PointOption.point(x+width/2, y-height/2)).release().perform();
+		   }
+  
+}
