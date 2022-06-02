2b84fa0b33167689344ef00f106897bb1054ba25
Biswajit-10 <https://github.com/Biswajit-10/A.git>
2022-06-02 23:01:13 +0530

    Keyboard

--- src/test/java/io/appium/TestClass.java
+++ src/test/java/io/appium/TestClass.java
@@ -14,6 +14,7 @@ import io.appium.java_client.ios.IOSDriver;
 public class TestClass {
 	public static void main(String[] args) {
 		IOSDriver<WebElement> driver=new IOSDriver(null);
+		driver.getConte
 		driver.hideKeyboard();
 //		ExtendedWebElement element=
 		WebElement element=driver.findElementById("");
